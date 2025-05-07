01e9cda567a5b58f9a77bcbd2dc5e9e2bc6941be
AyaAtK <149384859+AyaAtK@users.noreply.github.com>
2025-05-07 21:50:52 +0200

    Web.vue

--- vue-app/src/components/button-footer.vue
+++ vue-app/src/components/button-footer.vue
@@ -1,50 +1,47 @@
+<template>
+  <div>
+    <h1>Visor de Imágenes</h1>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Usuario</th>
+          <th>Fecha</th>
+          <th>Colores</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in datos" :key="index">
+          <td>{{ item.nombre }}</td>
+          <td>{{ item.usuario }}</td>
+          <td>{{ item.fecha }}</td>
+          <td>{{ item.colores }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
 <script>
 export default {
-  name: 'ButtonFooter',
-  props: {
-    item: {
-      type: Object,
-      default() {},
-    },
-    className: {
-      type: String,
-      default: () => '',
-    },
-    label: {
-      type: String,
-      default: () => '',
-    },
-    dataIndex: {
-      type: Number,
-      default: () => null,
-    },
-    dataId: {
-      type: Number,
-    },
-    iconClasses: {
-      type: String,
-      default: () => '',
-    },
-  },
-  methods: {
-    handleClick() {
-      this.$emit('clicked', this.item);
-    },
+  name: "HelloWorld",
+  data() {
+    return {
+      datos: [
+        {
+          nombre: "imagen1.png",
+          usuario: "juan123",
+          fecha: "2025-05-07 12:00",
+          colores: 15423,
+        },
+        {
+          nombre: "foto2.jpg",
+          usuario: "ana456",
+          fecha: "2025-05-06 18:22",
+          colores: 13201,
+        },
+      ],
+    };
   },
 };
 </script>
-
-<template>
-  <button
-    class="link card-footer-item"
-    :class="className"
-    :aria-label="label"
-    tabindex="0"
-    @click="handleClick"
-    :data-index="dataIndex"
-    :data-id="dataId"
-  >
-    <i :class="iconClasses"></i>
-    <span>{{ label }}</span>
-  </button>
-</template>
